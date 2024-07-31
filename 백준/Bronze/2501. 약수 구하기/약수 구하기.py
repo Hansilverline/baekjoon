9e0b366ad41c2f1ed11f97c737964539d301e47a
Hansilverline <158063910+Hansilverline@users.noreply.github.com>
2024-07-31 14:34:23 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,14 @@
+n,k = map(int,input().split())
+
+yaksu =[]
+
+for i in range(1,n+1):
+    if n%i == 0 :
+        yaksu.append(i)
+    else:
+        pass
+
+if len(yaksu) < k :
+    print(0)
+else : 
+    print(yaksu[k-1])
