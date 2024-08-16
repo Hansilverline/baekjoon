acdd59c5da6bceac2b0d4afba88e00d31974dff1
Hansilverline <158063910+Hansilverline@users.noreply.github.com>
2024-08-17 00:19:55 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ Python/백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,17 +1,11 @@
 n = int(input())
-result,count = 0,0
-old_n = n
-new_n = 0
+num = n
+count = 0
 
-if n == 0 :
-    count = 1
-while(new_n != n) :
-    if old_n < 10 : 
-        result = 0+old_n
-    else :
-        result = (old_n//10) + (old_n%10)
-    new_n = (old_n%10) * 10 + (result%10)
+while(1) :
+    c = (num//10) + (num%10)
+    num = (num%10)*10 + (c%10)
     count += 1
-    old_n=new_n
-
+    if num==n:
+        break
 print(count)
