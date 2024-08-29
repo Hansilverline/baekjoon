fb55f3b36030518fb20b47dfd8c313d0225f2567
Hansilverline <158063910+Hansilverline@users.noreply.github.com>
2024-08-29 17:03:54 +0900

    [Bronze V] Title: 검증수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,10 +1,8 @@
 numbers = list(map(int,input().split()))
 
-# [0,4,2,5,6]
 sum = 0
 
-for i in range(len(numbers)) :
-    sum += (numbers[i]**2)
-    last = sum % 10
+for number in numbers :
+    sum += (number**2)
 
-print(last)
+print(sum%10)
