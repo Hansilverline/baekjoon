e0c26816180ac112f8b68523eeea1fb2b524d647
Hansilverline <158063910+Hansilverline@users.noreply.github.com>
2024-10-06 14:08:13 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -1,4 +1,13 @@
-a,b = input().split()
-a = int(a[::-1]) 
-b = int(b[::-1]) 
-print(int(str(a+b)[::-1])) 
+x,y = input().split()
+
+def Rev(a) :
+    a = list(a)
+    reversed_list = [] 
+    for i in a[::-1]:
+        reversed_list.append(i)
+    result = ''.join(map(str, reversed_list))
+    return result
+
+lastPara = int(Rev(x))+int(Rev(y))
+result = Rev(str(lastPara))
+print(int(result))
